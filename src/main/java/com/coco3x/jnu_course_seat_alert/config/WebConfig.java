b1367b555ba758de837fe1200dce5eb6dc183ac4
coco3x <inferior3x@gmail.com>
2023-12-07 03:05:23 +0900

    add: WebConfig 추가
    페이지 컨트롤러와 RestController를 구분하기 위해 rest의 prefix를 /api로 설정

---
+++ src/main/java/com/coco3x/jnu_course_seat_alert/config/WebConfig.java
@@ -0,0 +1,14 @@
+package com.coco3x.jnu_course_seat_alert.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        configurer.addPathPrefix("/api", c -> c.isAnnotationPresent(RestController.class));
+    }
+}
