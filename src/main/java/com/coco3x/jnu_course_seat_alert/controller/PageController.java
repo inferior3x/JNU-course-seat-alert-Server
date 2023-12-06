1cd82dbe673db135785eebcac8e7fd75e3fea4d9
coco3x <inferior3x@gmail.com>
2023-12-07 02:52:26 +0900

    comment: 만들 페이지들 주석으로 표시

---
+++ src/main/java/com/coco3x/jnu_course_seat_alert/controller/PageController.java
@@ -0,0 +1,10 @@
+package com.coco3x.jnu_course_seat_alert.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class PageController {
+    //회원가입 페이지
+    //로그인 페이지
+    //강의 페이지
+}
