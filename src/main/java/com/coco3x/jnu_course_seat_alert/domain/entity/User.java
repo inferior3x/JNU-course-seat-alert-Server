8a8c909f41535a61f22f995275f01b2e18c0bd5c
coco3x <inferior3x@gmail.com>
2023-12-07 03:24:24 +0900

    add: User 엔티티 추가

---
+++ src/main/java/com/coco3x/jnu_course_seat_alert/domain/entity/User.java
@@ -0,0 +1,16 @@
+package com.coco3x.jnu_course_seat_alert.domain.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String userId;
+
+    @Column(nullable = false)
+    private String password;
+}
