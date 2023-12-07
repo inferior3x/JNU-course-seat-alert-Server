0d66cb765865e10d5aab2b52f9d42b91c9113e10
coco3x <inferior3x@gmail.com>
2023-12-07 11:06:18 +0900

    fix: 권한 확인하는 인터셉터 추가

--- src/main/java/com/coco3x/jnu_course_seat_alert/config/WebConfig.java
+++ src/main/java/com/coco3x/jnu_course_seat_alert/config/WebConfig.java
@@ -1,7 +1,9 @@
 package com.coco3x.jnu_course_seat_alert.config;
 
+import com.coco3x.jnu_course_seat_alert.interceptor.AuthorizationCheckInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,5 +12,15 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void configurePathMatch(PathMatchConfigurer configurer) {
         configurer.addPathPrefix("/api", c -> c.isAnnotationPresent(RestController.class));
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new AuthorizationCheckInterceptor())
+                .addPathPatterns("/api/**")
+                .excludePathPatterns(
+                        "/api/user/login",
+                        "/api/user/signup"
+                );
     }
 }
