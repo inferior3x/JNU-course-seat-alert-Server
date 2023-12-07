efc1f400a1c7b547ff87cba798f5201b47797732
coco3x <inferior3x@gmail.com>
2023-12-07 13:33:08 +0900

    fix: 로그인 아이디/비밀번호 정규표현식 검증 삭제
    로그인 아이디/비밀번호는 값이 없거나 공백인 것만 확인하면 된다고 판단했기 때문

--- src/main/java/com/coco3x/jnu_course_seat_alert/domain/dto/request/LogInReqDTO.java
+++ src/main/java/com/coco3x/jnu_course_seat_alert/domain/dto/request/LogInReqDTO.java
@@ -10,10 +10,8 @@ import lombok.Getter;
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class LogInReqDTO {
     @NotBlank(message = "아이디는 공백일 수 없습니다.")
-    @Pattern(regexp = "^[a-zA-Z0-9]{4,12}$", message = "아이디는 4~12글자의 영어, 숫자여야 합니다.")
     private String userId;
 
     @NotBlank(message = "비밀번호는 공백일 수 없습니다.")
-    @Pattern(regexp = "^[a-zA-Z0-9!@#$%^&*()_+=-]{4,12}$", message = "비밀번호는 4~12글자의 영어, 숫자, 특수문자여야 합니다.")
     private String password;
 }
