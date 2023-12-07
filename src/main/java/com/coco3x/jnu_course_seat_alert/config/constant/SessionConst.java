73dc67dcfdca4d75a74c6efbca414387afc4b306
coco3x <inferior3x@gmail.com>
2023-12-07 11:24:39 +0900

    add: 세션 속성 이름을 정적 상수로 가지는 클래스 추가

---
+++ src/main/java/com/coco3x/jnu_course_seat_alert/config/constant/SessionConst.java
@@ -0,0 +1,6 @@
+package com.coco3x.jnu_course_seat_alert.config.constant;
+
+public class SessionConst {
+    public static final String ID = "id";
+    public static final String AUTHORIZATION = "authorization";
+}
