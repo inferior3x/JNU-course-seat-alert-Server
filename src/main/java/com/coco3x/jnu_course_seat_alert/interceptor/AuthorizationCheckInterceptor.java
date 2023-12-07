26588c80674efbf3f185f116fe9f7e0520fce929
coco3x <inferior3x@gmail.com>
2023-12-07 11:02:36 +0900

    feat: 인증 권한이 있는지 확인할 인터셉터 정의

---
+++ src/main/java/com/coco3x/jnu_course_seat_alert/interceptor/AuthorizationCheckInterceptor.java
@@ -0,0 +1,19 @@
+package com.coco3x.jnu_course_seat_alert.interceptor;
+
+import com.coco3x.jnu_course_seat_alert.config.constant.SessionConst;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import java.util.Optional;
+
+public class AuthorizationCheckInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        HttpSession session = request.getSession();
+        Optional.ofNullable(session.getAttribute(SessionConst.AUTHORIZATION))
+                .orElseThrow(() -> new IllegalAccessException("로그인이 필요합니다."));
+        return true;
+    }
+}
