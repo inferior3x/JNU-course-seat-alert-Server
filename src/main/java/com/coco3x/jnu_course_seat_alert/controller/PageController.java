d79d6ff6a063c53466238a398510d5a9a39ea727
coco3x <inferior3x@gmail.com>
2023-12-07 14:43:54 +0900

    feat: 회원가입, 로그인, 강의, 설정 페이지 추가

--- src/main/java/com/coco3x/jnu_course_seat_alert/controller/PageController.java
+++ src/main/java/com/coco3x/jnu_course_seat_alert/controller/PageController.java
@@ -1,10 +1,20 @@
 package com.coco3x.jnu_course_seat_alert.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PageController {
     //회원가입 페이지
+    @GetMapping("/signup")
+    public void signup(){}
     //로그인 페이지
+    @GetMapping("/login")
+    public void login(){}
     //강의 페이지
+    @GetMapping("/course")
+    public void course(){}
+    //설정 페이지
+    @GetMapping("/setting")
+    public void setting(){}
 }
