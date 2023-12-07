b4dd7f16496942c9f23a62b65af2b0980cb6f635
coco3x <inferior3x@gmail.com>
2023-12-07 10:51:38 +0900

    feat: GlobalExceptionHandler 추가
    컨트롤러에서 발생하는 예외를 전역으로 처리하기 위해

---
+++ src/main/java/com/coco3x/jnu_course_seat_alert/controller/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.coco3x.jnu_course_seat_alert.controller;
+
+import com.coco3x.jnu_course_seat_alert.config.apiresponse.ApiResponse;
+import com.coco3x.jnu_course_seat_alert.config.apiresponse.ApiResponseCreator;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    //유효하지 않은 body
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ApiResponse<?> handleHttpMessageNotReadableException(HttpMessageNotReadableException e){
+        return ApiResponseCreator.fail("유효하지 않은 요청입니다.");
+    }
+    //@Valid 검증 실패
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ApiResponse<?> handleMethodArgumentNotValidException(MethodArgumentNotValidException e){
+        if (e.getBindingResult().getFieldError() != null)
+            return ApiResponseCreator.fail(e.getBindingResult().getFieldError().getDefaultMessage());
+        else
+            return ApiResponseCreator.fail("유효하지 않은 입력입니다.");
+    }
+}
+
