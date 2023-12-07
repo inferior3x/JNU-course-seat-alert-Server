2f3fa231e0758b4707d1138fe3e6b2a481a88b6d
coco3x <inferior3x@gmail.com>
2023-12-07 11:17:27 +0900

    feat: 세션 값을 편하게 꺼내오기 위한 어노테이션 정의

---
+++ src/main/java/com/coco3x/jnu_course_seat_alert/config/annotation/Session.java
@@ -0,0 +1,12 @@
+package com.coco3x.jnu_course_seat_alert.config.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Session {
+    String attr();
+}
