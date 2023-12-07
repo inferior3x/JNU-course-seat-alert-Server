49392f9d597affab7943f3a056d6e4db37592f6f
coco3x <inferior3x@gmail.com>
2023-12-07 11:20:52 +0900

    fix: 세션 어노테이션의 아규먼트 리졸버 추가

--- src/main/java/com/coco3x/jnu_course_seat_alert/config/WebConfig.java
+++ src/main/java/com/coco3x/jnu_course_seat_alert/config/WebConfig.java
@@ -1,11 +1,15 @@
 package com.coco3x.jnu_course_seat_alert.config;
 
+import com.coco3x.jnu_course_seat_alert.config.annotation.SessionMethodArgumentHandler;
 import com.coco3x.jnu_course_seat_alert.interceptor.AuthorizationCheckInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -22,5 +26,10 @@ public class WebConfig implements WebMvcConfigurer {
                         "/api/user/login",
                         "/api/user/signup"
                 );
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new SessionMethodArgumentHandler());
     }
 }
