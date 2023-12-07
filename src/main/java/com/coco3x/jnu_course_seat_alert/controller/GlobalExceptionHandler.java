e545c028521043a7cbdb42f9b20e79e35bc4165e
coco3x <inferior3x@gmail.com>
2023-12-07 11:35:38 +0900

    fix: 예외 처리(유니크 제약 위배) 추가

--- src/main/java/com/coco3x/jnu_course_seat_alert/controller/GlobalExceptionHandler.java
+++ src/main/java/com/coco3x/jnu_course_seat_alert/controller/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.coco3x.jnu_course_seat_alert.controller;
 
 import com.coco3x.jnu_course_seat_alert.config.apiresponse.ApiResponse;
 import com.coco3x.jnu_course_seat_alert.config.apiresponse.ApiResponseCreator;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -27,6 +28,11 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(IllegalAccessException.class)
     public ApiResponse<?> handleIllegalAccessException(IllegalAccessException e){
         return ApiResponseCreator.fail(e.getMessage());
+    }
+    //유니크 조건 위배
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ApiResponse<?> handleDataIntegrityViolationException(){
+        return ApiResponseCreator.fail("문제가 발생하였습니다. 다시 시도해주세요.");
     }
 }
 
