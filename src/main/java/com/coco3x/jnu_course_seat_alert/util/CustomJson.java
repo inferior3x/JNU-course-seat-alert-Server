9a531d112ca2e578bacc63584b88cb82c2bb3c0e
coco3x <inferior3x@gmail.com>
2023-12-08 18:44:19 +0900

    add: JsonNode로 변환하는 메서드 추가

--- src/main/java/com/coco3x/jnu_course_seat_alert/util/CustomJson.java
+++ src/main/java/com/coco3x/jnu_course_seat_alert/util/CustomJson.java
@@ -1,12 +1,16 @@
 package com.coco3x.jnu_course_seat_alert.util;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class CustomJson {
     public static ObjectMapper objectMapper = new ObjectMapper();
     public static <T> T convertJsonString2Object(String jsonString, Class<T> clazz) throws JsonProcessingException{
         return objectMapper.readValue(jsonString, clazz);
+    }
+    public static JsonNode convertJsonString2JsonNode(String jsonString) throws JsonProcessingException{
+        return objectMapper.readTree(jsonString);
     }
     public static String convertObject2JsonString(Object objectToBeConverted) throws JsonProcessingException{
         return objectMapper.writeValueAsString(objectToBeConverted);
