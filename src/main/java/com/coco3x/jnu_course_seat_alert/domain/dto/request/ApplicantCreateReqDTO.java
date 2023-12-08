42778ebaa123cf2ef08e62f8f48a1ad3a5f1bc42
coco3x <inferior3x@gmail.com>
2023-12-08 15:30:21 +0900

    fix: @Valid 검증될 수 있도록 수정, 스네이크 케이스 받을 수 있도록 수정

--- src/main/java/com/coco3x/jnu_course_seat_alert/domain/dto/request/ApplicantCreateReqDTO.java
+++ src/main/java/com/coco3x/jnu_course_seat_alert/domain/dto/request/ApplicantCreateReqDTO.java
@@ -1,11 +1,19 @@
 package com.coco3x.jnu_course_seat_alert.domain.dto.request;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 @Getter
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class ApplicantCreateReqDTO {
+    @NotBlank(message = "Invalid code")
     private String code;
+    @NotBlank(message = "Invalid name")
     private String name;
-    private int grade;
-    private int courseType;
+    @NotBlank(message = "Invalid grade")
+    private String grade;
+    @NotBlank(message = "Invalid course type")
+    private String courseType;
 }
