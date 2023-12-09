2e259feaf945f04c82d5388ce1dba4b0a895e77d
ajy4304 <ajy4304@naver.com>
2023-12-09 21:10:20 +0900

    feat(static): 로그인, 회원가입 api 호출 기능 추가

---
+++ src/main/resources/static/js/signup.js
@@ -0,0 +1,42 @@
+const idElement = document.querySelector("#user_id");
+const pwElement = document.querySelector("#password");
+const pw2Element = document.querySelector("#password2");
+const pushTokenElement = document.querySelector("#push_token");
+const signupBtnElement = document.querySelector("#spotlight-btn");
+
+const signup = _.throttle(async () => {
+    if (pwElement.value !== pw2Element.value){
+        showOkModal("비밀번호가 서로 다릅니다.", ()=>{});
+        return;
+    }
+
+    const bodyData = {
+        'user_id': idElement.value,
+        'password': pwElement.value,
+    };
+
+    showSpinner();
+    await fetchByPost("/api/user/signup",
+        bodyData,
+        () => { //auto login
+            showOkModal("가입되었습니다.",
+                async () => {
+                    if (pwElement.value !== ""){
+                        showOkModal("모바일 기기로 이용해주세요.");
+                        return;
+                    }
+                    bodyData['push_token'] = pushTokenElement.value;
+                    await fetchByPost("/api/user/login",
+                        bodyData,
+                        () => {window.location.href = "/course"},
+                        (responseData) => {showOkModal(responseData.data.message)},
+                    );
+                }
+            );
+        },
+        (responseData) => {showOkModal(responseData.data.message)},
+    );
+    hideSpinner();
+}, 1500);
+
+signupBtnElement.addEventListener("click", signup);
