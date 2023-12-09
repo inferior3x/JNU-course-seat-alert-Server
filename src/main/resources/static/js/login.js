8ca2273ce388d8eb091a2e4f1bd0e6776dfb3fb1
ajy4304 <ajy4304@naver.com>
2023-12-09 21:10:20 +0900

    feat(static): 로그인, 회원가입 api 호출 기능 추가

---
+++ src/main/resources/static/js/login.js
@@ -0,0 +1,27 @@
+const idElement = document.querySelector("#user_id");
+const pwElement = document.querySelector("#password");
+const pushTokenElement = document.querySelector("#push_token");
+const loginBtnElement = document.querySelector("#spotlight-btn");
+
+const login = _.throttle(async () => {
+    if (pwElement.value !== ""){
+        showOkModal("모바일 기기로 이용해주세요.");
+        return;
+    }
+    const bodyData = {
+        'user_id': idElement.value,
+        'password': pwElement.value,
+        'push_notification_id': pushTokenElement.value
+    };
+
+    showSpinner();
+    await fetchByPost("/api/user/login",
+        bodyData,
+        () => {window.location.href = "/course"},
+        (responseData) => {showOkModal(responseData.data.message)},
+    );
+    hideSpinner();
+
+}, 1500);
+
+loginBtnElement.addEventListener("click", login);
